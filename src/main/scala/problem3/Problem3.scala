5b4e0aa31d79cfd9133814e13b174327e47430cd
takumi-n <takumi.chillout@gmail.com>
2017-07-21 20:36:21 +0900

    Solve problem3

--- src/main/scala/problem3/Problem3.scala
+++ src/main/scala/problem3/Problem3.scala
@@ -4,5 +4,11 @@ package problem3
   * Created by takumi on 2017/07/21.
   */
 object Problem3 extends App {
+  private val dictionary = Map(1 -> "one", 2 -> "two")
 
+  def translate(num: Option[Int]): Option[String] = num.flatMap(dictionary.get)
+
+  println(translate(Some(1)))
+  println(translate(Some(3)))
+  println(translate(None))
 }
