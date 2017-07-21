a6b3845e2a3dc23753ff6c04fd58e6b61f0f475f
takumi-n <takumi.chillout@gmail.com>
2017-07-21 20:31:57 +0900

    Solve problem2

--- src/main/scala/problem2/Problem2.scala
+++ src/main/scala/problem2/Problem2.scala
@@ -4,5 +4,12 @@ package problem2
   * Created by takumi on 2017/07/21.
   */
 object Problem2 extends App {
+  def getFirst(value: Option[String]): String = value match {
+    case Some("") | None => ""
+    case Some(x) => x.substring(0, 1)
+  }
 
+  println(getFirst(Some("hello")))
+  println(getFirst(Some("")))
+  println(getFirst(None))
 }
