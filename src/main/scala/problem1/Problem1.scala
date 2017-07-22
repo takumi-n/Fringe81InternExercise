8dd48b2301df3402436628d4767e643b66892604
takumi-n <takumi.chillout@gmail.com>
2017-07-22 10:28:29 +0900

    Solve Problem1

--- src/main/scala/problem1/Problem1.scala
+++ src/main/scala/problem1/Problem1.scala
@@ -4,7 +4,7 @@ package problem1
   * Created by takumi on 2017/07/21.
   */
 object Problem1 extends App {
-  def show(value: Option[String]): Unit = value.foreach(println)
+  def show(value: Option[String]): Unit = value.foreach(println(_))
 
   show(Some("hello"))
   show(None)
