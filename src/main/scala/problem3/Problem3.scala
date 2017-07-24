5a112292d6593b8f9dbece9ddccd91d6508cca4e
takumi-n <takumi.chillout@gmail.com>
2017-07-24 11:01:02 +0900

    Update Problem3

--- src/main/scala/problem3/Problem3.scala
+++ src/main/scala/problem3/Problem3.scala
@@ -8,7 +8,28 @@ object Problem3 extends App {
 
   def translate(num: Option[Int]): Option[String] = num flatMap { dictionary.get }
 
+  def translate1(num: Option[Int]): Option[String] = dictionary.get(num.getOrElse(-1)) // Bad
+
+  def translate2(num: Option[Int]): Option[String] = num match {
+    case Some(x) => dictionary.get(x)
+    case _ => None
+  }
+
+  def translate3(num: Option[Int]): Option[String] = (for {n <- num} yield dictionary.get(n)).flatten
+
   println(translate(Some(1)))
   println(translate(Some(3)))
   println(translate(None))
+
+  println(translate1(Some(1)))
+  println(translate1(Some(3)))
+  println(translate1(None))
+
+  println(translate2(Some(1)))
+  println(translate2(Some(3)))
+  println(translate2(None))
+
+  println(translate3(Some(1)))
+  println(translate3(Some(3)))
+  println(translate3(None))
 }
